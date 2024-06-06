663f9173212cf86c2c3d95bf413b5c1aa12112a4
RangiraDave <rangiradev666@gmail.com>
2024-06-06 20:17:22 +0200

    Made the file executable

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,11 +13,14 @@ Here is a method to open all the boxes:
 
 def canUnlockAll(boxes):
     """ Method to determine if all the boxes can be opened. """
+
     if not boxes:
         return False
+
     keys = [0]
     for key in keys:
         for new_key in boxes[key]:
             if new_key not in keys and new_key < len(boxes):
                 keys.append(new_key)
+
     return len(keys) == len(boxes)
