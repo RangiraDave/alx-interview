72486fe6cc04a4fcb2a4b574c0c43aea78c2c91b
RangiraDave <rangiradev666@gmail.com>
2024-06-20 18:57:37 +0200

    Corrected pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,24 +2,26 @@
 
 def minOperations(n):
     """
-    Function that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+    Function that calculates the fewest number of operations needed to
+    result in exactly n H characters in the file.
     Args:
         n (int): The number of characters to be printed.
     Returns:
-        int: The fewest number of operations needed to result in exactly n H characters in the file.
+        int: The fewest number of operations needed to result in
+        exactly n H characters in the file.
     """
 
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         if n % divisor == 0:
             operations += divisor
             n //= divisor
         else:
             divisor += 1
-    
+
     return operations
