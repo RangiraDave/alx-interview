d24e34c50cbcccccc6717b9b99cb42679ca234d2
RangiraDave <rangiradev666@gmail.com>
2024-06-20 18:54:15 +0200

    Minimal operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def minOperations(n):
+    """
+    Function that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+    Args:
+        n (int): The number of characters to be printed.
+    Returns:
+        int: The fewest number of operations needed to result in exactly n H characters in the file.
+    """
+
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    divisor = 2
+    
+    while n > 1:
+        if n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        else:
+            divisor += 1
+    
+    return operations
