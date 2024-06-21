e65917763d7cf2fa5f46eb12bee0d811332573d1
RangiraDave <rangiradev666@gmail.com>
2024-06-21 15:52:15 +0200

    made the file executable

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,7 +3,6 @@
 Log Parsing
 """
 
-import re
 import sys
 
 
@@ -11,7 +10,7 @@ def print_stats(total_size, status_codes):
     """
     Print the log stats
     """
-    print("Total file size: File size: {}".format(total_size))
+    print("File size: {}".format(total_size))
     for code in sorted(status_codes.keys()):
         if status_codes[code] > 0:
             print("{}: {}".format(code, status_codes[code]))
@@ -21,24 +20,30 @@ def parse_line(line):
     """
     Parse a line from the input
     """
-    line_format = re.compile(r'^\S+ - \[\S+ \S+\] "GET /projects/260 HTTP/1\.1" (\d{3}) (\d+)$')
-    match = line_format.match(line)
-    if match:
-        status_code = int(match.group(1))
-        file_size = int(match.group(2))
-        return status_code, file_size
-    return None, None
+    try:
+        parts = line.split()
+        ip_address = parts[0]
+        status_code = int(parts[-2])
+        file_size = int(parts[-1])
+        return ip_address, status_code, file_size
+    except (IndexError, ValueError):
+        return None, None, None
 
 
 def main():
+    """
+    Main function
+    """
     total_size = 0
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    status_codes = {
+        200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0
+        }
     line_count = 0
 
     try:
         for line in sys.stdin:
-            status_code, file_size = parse_line(line)
-            if status_code is None:
+            ip_address, status_code, file_size = parse_line(line)
+            if ip_address is None:
                 continue
 
             total_size += file_size
