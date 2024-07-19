12ee040dd745952b932c256f76c0793b5359f328
RangiraDave <rangiradev666@gmail.com>
2024-07-19 20:09:26 +0200

    Corrected the semistandard validation and added some comments

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,29 +2,22 @@
 
 const request = require('request');
 
-// Get the movie ID from command line arguments
 const movieId = process.argv[2];
 
-// Construct the URL for the movie API
 const url = `https://swapi-api.alx-tools.com/films/${movieId}`;
 
-// Make a request to the movie API
 request(url, (error, response, body) => {
     if (error) {
         console.error('Error:', error);
     } else {
-        // Parse the response body into a JSON object
         const film = JSON.parse(body);
         const characters = film.characters;
 
-        // Iterate over each character URL
         characters.forEach((characterUrl) => {
-            // Make a request to the character API
             request(characterUrl, (error, response, body) => {
                 if (error) {
                     console.error('Error:', error);
                 } else {
-                    // Parse the response body into a JSON object
                     const character = JSON.parse(body);
                     console.log(character.name);
                 }
