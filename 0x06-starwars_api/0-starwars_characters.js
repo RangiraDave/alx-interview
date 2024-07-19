75d84043b031139cf7bcdcdbdeafd5b33bcdc49e
RangiraDave <rangiradev666@gmail.com>
2024-07-19 20:06:01 +0200

    Corrected the semistandard validation

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,9 +2,12 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
-
+/**
+ * Sends a request to retrieve and display the names of characters from a Star Wars movie.
+ * @param {string[]} characterList - The list of character URLs.
+ * @param {number} index - The index of the current character in the list.
+ * @returns {void}
+ */
 function sendRequest(characterList, index) {
     if (characterList.length === index) {
         return;
@@ -20,6 +23,12 @@ function sendRequest(characterList, index) {
     });
 }
 
+/**
+ * Retrieves the character list for a given Star Wars movie and calls the sendRequest function.
+ * @param {string} movieId - The ID of the Star Wars movie.
+ * @returns {void}
+ */
+const movieEndpoint = 'https://swapi-api.hbtn.io/api/films/1/';
 request(movieEndpoint, (error, response, body) => {
     if (error) {
         console.log(error);
